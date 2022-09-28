2bfc71c10be0e73acc68c6674e8d1fc2a81d65b8
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2022-09-28 12:54:10 +0200

    0.41 Needed to Commit
    -No major changes

--- Assets/Scripts/Scores.cs
+++ Assets/Scripts/Scores.cs
@@ -9,15 +9,23 @@ public class Scores : MonoBehaviour
     public int Player2Score = 0;
     public Text p1Text;
     public Text p2Text;
-    
+
     public void AddP1Score()
     {
         Player1Score++;
         p1Text.text = Player1Score.ToString();
+        if (Player1Score == 1) ;
+        {
+            //            GameObject.Find("OhHeyAWinB").GetComponent<Bl>().BlueWins();
+        }
     }
     public void AddP2Score()
     {
         Player2Score++;
         p2Text.text = Player2Score.ToString();
+        if (Player2Score == 1)
+        {
+            use SceneManager,;LoadScene(BlueWin);
+        }
     }
 }
