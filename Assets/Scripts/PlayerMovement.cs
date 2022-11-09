58e9835aff0e132afd4b43d825d863acd6414d8a
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2022-11-09 22:16:49 +0100

    1.0 Pong is done
    Spinners exist now at 8 points
    Made some things look better

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int playerNumber = 1;
+    public int playerSpeed = 4;
     // Update is called once per frame
     void Update()
     {
         if (playerNumber == 1)
         {
-            transform.Translate(new Vector3(0, Input.GetAxis("Player1")* Time.deltaTime* 4, 0));
+            transform.Translate(new Vector3(0, Input.GetAxis("Player1")* Time.deltaTime* playerSpeed, 0));
         }
         if (playerNumber == 2)
         {
-            transform.Translate(new Vector3(0, Input.GetAxis("Player2")* Time.deltaTime* 4, 0));
+            transform.Translate(new Vector3(0, Input.GetAxis("Player2")* Time.deltaTime* playerSpeed, 0));
         }
         transform.position = new Vector3(transform.position.x, 
             Mathf.Clamp(transform.position.y, -3,3), transform.position.z);
--- Assets/Scripts/Scores.cs
+++ Assets/Scripts/Scores.cs
@@ -9,33 +9,52 @@ public class Scores : MonoBehaviour
     public int Player1Score = 0;
     public int Player2Score = 0;
     public int Player3Score = 0;
+    public int Player1win = 10;
+    public int Player2win = 10;
+    public int Player3win = 5;
+    public int Player1spin = 2;
+    public int Player2spin = 2;
     public Text p1Text;
     public Text p2Text;
     public Text p3Text;
+    [SerializeField]
+    GameObject Spinner;
+    [SerializeField]
+    Transform Tp1;
+    [SerializeField]
+    Transform Tp2;
 
     public void AddP1Score()
     {
         Player1Score++;
         p1Text.text = Player1Score.ToString();
-        if (Player1Score == 10)
+        if (Player1Score == Player1win)
         {
             SceneManager.LoadScene("BlueWin");
+        }
+        if (Player1Score == Player1spin)
+        {
+            Instantiate(Spinner, Tp1.position, Tp1.rotation);
         }
     }
     public void AddP2Score()
     {
         Player2Score++;
         p2Text.text = Player2Score.ToString();
-        if (Player2Score == 10)
+        if (Player2Score == Player2win)
         {
            SceneManager.LoadScene("RedWin");
+        }
+        if (Player2Score == Player2spin)
+        {
+            Instantiate(Spinner, Tp2.position, Tp2.rotation);
         }
     }
     public void AddP3Score()
     {
         Player3Score++;
         p3Text.text = Player3Score.ToString();
-        if (Player3Score == 5)
+        if (Player3Score == Player3win)
         {
             SceneManager.LoadScene("YellowWin");
         }
